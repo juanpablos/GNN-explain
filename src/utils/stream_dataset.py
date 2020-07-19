c0f8cd128f74ed3250acdc333381f170b62e7ecb
Juan-Pablo Silva <jpsilvapena@gmail.com>
2020-07-18 23:52:41 -0400

    Reduce generated graphs by storing previous

--- src/utils/stream_dataset.py
+++ src/utils/stream_dataset.py
@@ -1,11 +1,23 @@
+import numpy as np
 from torch.utils.data import IterableDataset
 
 
 class LimitedStreamDataset(IterableDataset):
-    def __init__(self, data, limit: int):
+    def __init__(self, data, limit: int, store: bool = False):
         self.data = data
         self.limit = limit
+        self.store = store
+        self.dataset = []
 
     def __iter__(self):
-        for _ in range(self.limit):
-            yield next(self.data)
+        if self.dataset:
+            np.random.shuffle(self.dataset)
+            yield from self.dataset
+        else:
+            for _ in range(self.limit):
+                datapoint = next(self.data)
+
+                if self.store:
+                    self.dataset.append(datapoint)
+
+                yield datapoint
