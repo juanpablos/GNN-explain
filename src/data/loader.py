3d4cb09ba87c58747df50b6464ccc0d733df4b79
Juan-Pablo Silva <jpsilvapena@gmail.com>
2020-09-20 22:54:35 -0300

    [WIP] Refactor of formula loader

--- src/data/loader.py
+++ src/data/loader.py
@@ -5,6 +5,7 @@ from typing import Dict, List
 from src.data.datasets import (
     AggregatedNetworkDataset,
     LabeledDataset,
+    LabeledSubset,
     NetworkDataset,
     NetworkDatasetCollectionWrapper
 )
@@ -15,7 +16,7 @@ from src.data.formulas.labeler import (
     MultiLabelCategoricalLabeler
 )
 from src.data.utils import label_idx2tensor
-from src.typing import S, Selectable, T
+from src.typing import S, T
 
 logger = logging.getLogger(__name__)
 
@@ -30,28 +31,22 @@ def __prepare_files(path: str):
     return files
 
 
-def load_gnn_files(
-        root: str,
-        model_hash: str,
-        selector: Filter,
-        labeler: LabelerApply[T, S],
-        formula_mapping: FormulaMapping,
-        test_selector: Filter,
-        load_aggregated: str = None,
-        _legacy_load_without_batch: bool = False):
+def __load_formulas(
+    root: str,
+    model_hash: str,
+    selector: Filter,
+    formula_mapping: FormulaMapping,
+    test_selector: Filter,
+    load_aggregated: str = None,
 
+):
     if model_hash not in os.listdir(root):
         raise FileExistsError(
             f"No directory for the current model hash: {root}/{model_hash}")
 
-    is_multilabel = False
-    if isinstance(labeler.labeler, MultiLabelCategoricalLabeler):
-        logger.debug("Using a multilabel labeler")
-        is_multilabel = True
-
     model_path = os.path.join(root, model_hash)
 
-    preloaded_formulas: Selectable = {}
+    preloaded = None
     if load_aggregated is None:
         # select all formulas available in directory
         # formula_hash -> file_path
@@ -59,11 +54,10 @@ def load_gnn_files(
 
     else:
         logging.info("Loading batch formulas")
-        _agg = AggregatedNetworkDataset(
+        preloaded = AggregatedNetworkDataset(
             file_path=os.path.join(model_path, load_aggregated))
 
-        available_formulas = _agg.available_formulas()
-        preloaded_formulas = _agg
+        available_formulas = preloaded.available_formulas()
 
     logger.debug("Creating formula objects")
     # mapping from formula_hash -> formula object
@@ -79,71 +73,106 @@ def load_gnn_files(
         logger.debug("Adding exclusive testing formulas")
         selected_formulas.update(testing_selected_formulas)
 
+    return model_path, selected_formulas, testing_selected_formulas, available_formulas, preloaded
+
+
+def categorical_loader(
+    root: str,
+    model_hash: str,
+    selector: Filter,
+    labeler: LabelerApply[T, S],
+    formula_mapping: FormulaMapping,
+    test_selector: Filter,
+    load_aggregated: str = None,
+    _legacy_load_without_batch: bool = False
+):
+
+    model_path, selected_formulas, \
+        testing_selected_formulas, \
+        available_formulas, preloaded = __load_formulas(
+            root=root,
+            model_hash=model_hash,
+            selector=selector,
+            formula_mapping=formula_mapping,
+            test_selector=test_selector,
+            load_aggregated=load_aggregated
+        )
+
     logger.debug(f"Running formula labeler {labeler}")
     # mapping from the selected
     #   single label: formula_hash -> label_id
     #   multilabel: formula_hash -> List[label_id]
     # classes is a dictionary label_id -> label_name
     selected_labels, classes = labeler(selected_formulas)
+    n_labels = len(classes)
+
+    # * if multilabel, selected_labels is remapped to
+    # * formula_hash -> multilabel tensor mask
+
+    is_multilabel = False
+    if isinstance(labeler.labeler, MultiLabelCategoricalLabeler):
+        logger.debug("Using a multilabel labeler")
+        is_multilabel = True
+        selected_labels = {
+            h: label_idx2tensor(l, n_labels=n_labels)
+            for h, l in selected_labels.items()
+        }
 
     # contains all formulas in use in the experiment
     datasets: List[NetworkDataset[T]] = []
 
     # contains formulas used for training when test manually selected
-    train_dataset: List[NetworkDataset[T]] = []
+    train_dataset: List[int] = []
     # contains formulas used for testing when test manually selected
-    test_dataset: List[NetworkDataset[T]] = []
+    test_dataset: List[int] = []
 
-    n_labels = len(classes)
-
-    logger.info(f"Loading {len(selected_labels)} formulas")
+    logger.info(f"Labeling {len(selected_labels)} formulas")
     for formula_hash, label in selected_labels.items():
         formula_object = selected_formulas[formula_hash]
 
-        logger.info(f"\tLoading {formula_hash}: {formula_object}: {label}")
-
-        if is_multilabel:
-            label = label_idx2tensor(label=label, n_labels=n_labels)
-
-        if load_aggregated is None:
+        if preloaded is None:
+            logger.info(f"\tLoading {formula_hash}: {formula_object}: {label}")
             file = available_formulas[formula_hash]
             file_path = os.path.join(model_path, file)
-            dataset = NetworkDataset(
-                file=file_path,
+            dataset = NetworkDataset.categorical(
                 label=label,
                 formula=formula_object,
+                file=file_path,
+                multilabel=is_multilabel,
                 _legacy_load_without_batch=_legacy_load_without_batch)
         else:
-            dataset = NetworkDataset(
-                preloaded=preloaded_formulas[formula_hash],
+            logger.info(
+                f"\tLabeling {formula_hash}: {formula_object}: {label}")
+            dataset = NetworkDataset.categorical(
                 label=label,
                 formula=formula_object,
-                _legacy_load_without_batch=_legacy_load_without_batch)
+                preloaded=preloaded[formula_hash],
+                multilabel=is_multilabel)
 
         if testing_selected_formulas:
+            current_len = len(datasets)
+            current_indices = [i + current_len for i in range(len(dataset))]
             if formula_hash in testing_selected_formulas:
-                test_dataset.append(dataset)
+                test_dataset.extend(current_indices)
             else:
-                train_dataset.append(dataset)
+                train_dataset.extend(current_indices)
 
         # we append all formulas here
         datasets.append(dataset)
 
+    dataset_all = LabeledDataset.from_iterable(
+        datasets, multilabel=is_multilabel)
+
     if testing_selected_formulas:
         assert len(test_dataset) > 0, "test_dataset is empty"
-        return ((LabeledDataset.from_iterable(train_dataset,
-                                              multilabel=is_multilabel),
-                 LabeledDataset.from_iterable(test_dataset,
-                                              multilabel=is_multilabel)),
-                classes,
-                selected_formulas,
-                selected_labels,
-                NetworkDatasetCollectionWrapper(test_dataset))
+        return_dataset = (
+            LabeledSubset(dataset=dataset_all, indices=train_dataset),
+            LabeledSubset(dataset=dataset_all, indices=test_dataset)
+        )
     else:
-        # when the test_set is not manually selected we return a
-        # big dataset containing all formulas
-        return (
-            LabeledDataset.from_iterable(datasets, multilabel=is_multilabel),
+        return_dataset = dataset_all
+
+    return (return_dataset,
             classes,
             selected_formulas,
             selected_labels,
