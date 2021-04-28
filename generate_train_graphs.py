5458765fa01b65f3f6401bf0b68303b5c6fdbeb5
Juan-Pablo Silva <jpsilvapena@gmail.com>
2021-04-28 01:28:09 -0400

    fix new graphs random state

--- generate_train_graphs.py
+++ generate_train_graphs.py
@@ -1,4 +1,8 @@
+import os
+import random
+
 import networkx as nx
+import numpy as np
 import torch
 import torch.nn.functional
 from torch_geometric.data import Data, InMemoryDataset
@@ -6,13 +10,13 @@ from torch_geometric.data import Data, InMemoryDataset
 from src.data.auxiliary import GNNGraphDataset
 from src.generate_graphs import graph_object_stream
 from src.graphs.foc import *
-import numpy as np
-import os
+
+rand = random.Random(42)
 
 
 save_path = os.path.join("data", "graphs")
 os.makedirs(save_path, exist_ok=True)
-filename = "test_graphs.pt"
+filename = "train_graphs"
 
 
 def graph_to_data(graph):
@@ -38,32 +42,41 @@ for a in range(5, 81, 5):
 
 data_config = {
     "generator_fn": "random",
-    "min_nodes": 80,  # 20
-    "max_nodes": 120,  # 80
-    "seed": 0,
+    "min_nodes": 20,  # 20 80
+    "max_nodes": 80,  # 80 120
+    # "seed": 0,
     "n_properties": 4,
     "property_distribution": "manual",
     # "distribution": [v / 100.0 for v in tuples[0]],
     "verbose": 0,
-    "number_of_graphs": 1,
+    "number_of_graphs": 200,
     # --- generator config
     "name": "erdos",
     # ! because the generated graph is undirected,
     # ! the number of average neighbors will be double `m`
-    "m": 4,
+    # "m": 4,
 }
 
 total = len(color_tuples)
 graph_data = []
-for i, color_tuple in enumerate(color_tuples):
-    print(f"{i + 1}/{total}", color_tuple)
-    graph_generator = graph_object_stream(**data_config, distribution=color_tuple)
 
-    graph_data.extend(graph_to_data(g) for g in graph_generator)
+
+# train: 200 graphs, m=4, color distributions 5->80
+# test: 1 graph, m=[2, 4, 5], color distributions 5-> 80
+
+
+for m in [4]:
+    for i, color_tuple in enumerate(color_tuples):
+        print(f"m={m}, {i + 1}/{total}", color_tuple)
+        graph_generator = graph_object_stream(
+            **data_config, distribution=color_tuple, m=m, seed=rand.randint(1, 1 << 30)
+        )
+
+        graph_data.extend(graph_to_data(g) for g in graph_generator)
 
 np.random.shuffle(graph_data)
 data, slices = InMemoryDataset.collate(graph_data)
 dataset = GNNGraphDataset(data=data, slices=slices)
 
-torch.save(dataset, os.path.join(save_path, filename))
+torch.save(dataset, os.path.join(save_path, f"{filename}_{len(dataset)}.pt"))
 print(dataset)
