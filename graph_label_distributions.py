eff716cfba7908d613e6da2b1d94a90475e5f80d
Juan-Pablo Silva <jpsilvapena@gmail.com>
2021-05-30 15:41:39 -0400

    add 1% positivity distribution plots

--- graph_label_distributions.py
+++ graph_label_distributions.py
@@ -30,11 +30,13 @@ def get_existing_hashes(path: str):
 
 
 results_path = os.path.join("results", "analysis", "graph_distributions")
+results_path_1 = os.path.join(results_path, "1up")
 results_path_10 = os.path.join(results_path, "10up")
 results_path_20 = os.path.join(results_path, "20up")
 results_path_distributions = os.path.join(results_path, "distributions")
 
 os.makedirs(results_path, exist_ok=True)
+os.makedirs(results_path_1, exist_ok=True)
 os.makedirs(results_path_10, exist_ok=True)
 os.makedirs(results_path_20, exist_ok=True)
 os.makedirs(results_path_distributions, exist_ok=True)
@@ -44,11 +46,14 @@ mapping = FormulaMapping(os.path.join("data", "formulas.json"))
 
 all_hashes = get_formula_hashes(path=graph_labels_path)
 
-existing_hashes = get_existing_hashes(path=results_path_10) & get_existing_hashes(
-    path=results_path_20
+existing_hashes = (
+    get_existing_hashes(path=results_path_1)
+    & get_existing_hashes(path=results_path_10)
+    & get_existing_hashes(path=results_path_20)
 )
 
 stats_mapping = {}
+percent_1 = {}
 percent_10 = {}
 percent_20 = {}
 
@@ -70,22 +75,28 @@ for formula_hash in all_hashes:
 
     total = 0
     all_0s = 0
+    percent_1up = 0
     percent_10up = 0
     percent_20up = 0
 
     all_distributions = set()
+    relevant_distributions1 = defaultdict(int)
     relevant_distributions10 = defaultdict(int)
     relevant_distributions20 = defaultdict(int)
     for distribution, distribution_labels in train_labels.items():
         for labels in distribution_labels:
             all_0s += int(np.sum(labels) == 0)
 
+            relevant1 = bool(np.mean(labels) >= 0.01)
             relevant10 = bool(np.mean(labels) >= 0.1)
             relevant20 = bool(np.mean(labels) >= 0.2)
 
+            percent_1up += int(relevant1)
             percent_10up += int(relevant10)
             percent_20up += int(relevant20)
 
+            if relevant1:
+                relevant_distributions1[str(distribution)] += int(relevant1)
             if relevant10:
                 relevant_distributions10[str(distribution)] += int(relevant10)
             if relevant20:
@@ -99,15 +110,21 @@ for formula_hash in all_hashes:
         "formula_repr": repr(formula),
         "all_0s%": float(all_0s) / total,
         "all_0s": all_0s,
+        "1%+%": float(percent_1up) / total,
+        "1%+": percent_1up,
         "10%+%": float(percent_10up) / total,
         "10%+": percent_10up,
         "20%+%": float(percent_20up) / total,
         "20%+": percent_20up,
         "total": total,
     }
+    percent_1[formula_hash] = percent_1up
     percent_10[formula_hash] = percent_10up
     percent_20[formula_hash] = percent_20up
 
+    relevant_count_1up = [
+        relevant_distributions1[distribution] for distribution in all_distributions
+    ]
     relevant_count_10up = [
         relevant_distributions10[distribution] for distribution in all_distributions
     ]
@@ -115,10 +132,22 @@ for formula_hash in all_hashes:
         relevant_distributions20[distribution] for distribution in all_distributions
     ]
 
+    n, bins, patches = ax1.hist(relevant_count_1up, 30, facecolor="g")
+    ax1.set_title(
+        f"{str(formula)}: total {percent_1up} graphs ({float(percent_1up) / total:.2%})"
+    )
+    ax1.set_xlim((0, 200))  # 200 graphs per distribution
+    ax1.set_xlabel("N graphs with 1%+ positives for formula")
+    fig.tight_layout()
+    fig.savefig(os.path.join(results_path_1, f"{formula_hash}.png"))
+
+    ax1.clear()
+
     n, bins, patches = ax1.hist(relevant_count_10up, 30, facecolor="g")
     ax1.set_title(
         f"{str(formula)}: total {percent_10up} graphs ({float(percent_10up) / total:.2%})"
     )
+    ax1.set_xlim((0, 200))  # 200 graphs per distribution
     ax1.set_xlabel("N graphs with 10%+ positives for formula")
     fig.tight_layout()
     fig.savefig(os.path.join(results_path_10, f"{formula_hash}.png"))
@@ -129,12 +158,19 @@ for formula_hash in all_hashes:
     ax1.set_title(
         f"{str(formula)}: total {percent_20up} graphs ({float(percent_20up) / total:.2%})"
     )
+    ax1.set_xlim((0, 200))  # 200 graphs per distribution
     ax1.set_xlabel("N graphs with 20%+ positives for formula")
     fig.tight_layout()
     fig.savefig(os.path.join(results_path_20, f"{formula_hash}.png"))
 
     ax1.clear()
 
+    with open(
+        os.path.join(results_path_distributions, f"{formula_hash}_1up.json"),
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(relevant_distributions1, f, ensure_ascii=False, indent=2)
     with open(
         os.path.join(results_path_distributions, f"{formula_hash}_10up.json"),
         "w",
@@ -155,6 +191,17 @@ with open(
 
 
 total = next(iter(stats_mapping.values()))["total"]
+
+_min = min(percent_1.values())
+_max = max(percent_1.values())
+
+percent_1["min"] = _min
+percent_1["min_complement"] = total - _min
+percent_1["max"] = _max
+percent_1["max_complement"] = total - _max
+percent_1["total"] = total
+with open(os.path.join(results_path, "percent_1.json"), "w", encoding="utf-8") as f:
+    json.dump(percent_1, f, ensure_ascii=False, indent=2)
 
 _min = min(percent_10.values())
 _max = max(percent_10.values())
