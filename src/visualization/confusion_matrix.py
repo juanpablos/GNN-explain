5b1b282fd8d27e08eb68afd7de5cce0cfb096d63
Juan-Pablo Silva <jpsilvapena@gmail.com>
2021-06-26 23:34:17 -0400

    Improve multilabel confusion matrix plot

--- src/visualization/confusion_matrix.py
+++ src/visualization/confusion_matrix.py
@@ -92,45 +92,57 @@ def plot_multilabel_confusion_matrix(
 ):
     logger.info(f"Calculating multilabel confusion matrix")
 
-    cm_array = multilabel_confusion_matrix(y, y_pred)
+    confusion_matrices = multilabel_confusion_matrix(y, y_pred)
     size = len(labels) * 3
 
     n_rows = math.ceil(math.sqrt(len(labels)))
     n_cols = math.ceil(len(labels) / n_rows)
-    fig, axs = plt.subplots(ncols=int(n_cols), nrows=int(n_rows), figsize=(size, size))
 
-    axs = axs.flatten()  # type: ignore
+    fig = plt.figure(constrained_layout=True, figsize=(size * 2, size))
+    subfigs = fig.subfigures(n_rows, n_cols, wspace=0.07).flatten()
 
     logger.debug("Plotting confusion matrices")
-    for i, (cm, ax) in enumerate(zip(cm_array, axs)):
+    for i, (subfig, cm) in enumerate(zip(subfigs, confusion_matrices)):
+        ax1, ax2 = subfig.subplots(1, 2, sharey=True)
 
-        norm_cm = cm / cm.sum(axis=1, keepdims=True)
+        precision_cm = cm / cm.sum(axis=0, keepdims=True)
+        recall_cm = cm / cm.sum(axis=1, keepdims=True)
 
-        rest_label = f"rest ({sum(label_totals) - label_totals[i]})"
         current_label = f"{labels[i]} ({label_totals[i]})"
+        rest_label = f"rest ({sum(label_totals) - label_totals[i]})"
 
         label_names = [rest_label, current_label]
-        disp = ConfusionMatrixDisplay(
-            confusion_matrix=norm_cm, display_labels=label_names
+        precision_disp = ConfusionMatrixDisplay(
+            confusion_matrix=precision_cm, display_labels=label_names
+        )
+        recall_disp = ConfusionMatrixDisplay(
+            confusion_matrix=recall_cm, display_labels=label_names
         )
 
         logger.debug(f"Plotting for {labels[i]}")
-        disp.plot(cmap="Blues", ax=ax, xticks_rotation=30)
+        recall_disp.plot(cmap="Blues", ax=ax1, xticks_rotation=30, colorbar=False)
+        precision_disp.plot(cmap="Blues", ax=ax2, xticks_rotation=30, colorbar=False)
 
         plt.setp(
-            ax.get_xticklabels(),
+            ax1.get_xticklabels(),
             rotation=30,  # type:ignore
             horizontalalignment="right",
         )
-        disp.im_.set_clim(0, 1)
+        plt.setp(
+            ax2.get_xticklabels(),
+            rotation=30,  # type:ignore
+            horizontalalignment="right",
+        )
 
-        ax.set_title(f"CM for all vs {labels[i]}")
+        ax1.set_title(f"Recall")
+        ax2.set_title(f"Precision")
+
+        subfig.suptitle(f"CM for all vs {labels[i]}")
 
     figure_name = filename if filename is not None else "confusion_matrix"
     figure_title = title if title is not None else ""
 
     plt.suptitle(figure_title, wrap=True)
-    plt.tight_layout()
 
     os.makedirs(f"{save_path}/cm/", exist_ok=True)
     plt.savefig(f"{save_path}/cm/{figure_name}.png")
