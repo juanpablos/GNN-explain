0a9db3c1e5b72a6ff7af7d7fbf5b37da26714e92
Juan-Pablo Silva <jpsilvapena@gmail.com>
2021-08-08 15:39:53 -0400

    add categorical labels to umap

--- src/visualization/umap.py
+++ src/visualization/umap.py
@@ -1,20 +1,26 @@
+import os
+from typing import Dict
+
+import matplotlib.pyplot as plt
 import umap
 import umap.plot
-import matplotlib.pyplot as plt
-import os
 
 
 def plot_embedding_2d(
     embedding,
     labels,
+    labels_categorical_mapping: Dict[int, str],
     save_path: str,
     filename: str,
     seed=None,
 ):
     fig, ax = plt.subplots(figsize=(20, 10))
 
-    mapper = umap.UMAP(random_state=seed).fit(embedding)
-    umap.plot.points(mapper, labels=labels, ax=ax, width=1600, height=1000)
+    mapper = umap.UMAP(random_state=seed, low_memory=False).fit(embedding)
+
+    string_labels = [labels_categorical_mapping[label] for label in labels]
+
+    umap.plot.points(mapper, labels=string_labels, ax=ax, width=1600, height=1000)
 
     embedding_path = os.path.join(save_path, "embedding")
     os.makedirs(embedding_path, exist_ok=True)
